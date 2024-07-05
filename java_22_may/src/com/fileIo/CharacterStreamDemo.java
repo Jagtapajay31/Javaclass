bef70f318945e7a66be18ecb1e8229867ada6b0a
ajay jagtap <jagtapajay64@gmail.com>
2024-07-05 11:56:02 +0530

    new

---
+++ java_22_may/src/com/fileIo/CharacterStreamDemo.java
@@ -0,0 +1,48 @@
+package com.fileIo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * 2. Character stream
+ * 		1.FileWriter
+ * 			to write data into the file
+ * 		2.FileReader
+ * 			to read data from the file
+ * 
+ */
+public class CharacterStreamDemo {
+	public static void main(String[] args) {
+		
+		try {
+			//to open and create file
+			FileWriter fw=new FileWriter("file2.txt",true);
+			String msg="\nThis is Top Texh Surt Ring Road";
+			//to write into the file
+			fw.write(msg);
+			//3. to close file
+			fw.close();
+			System.out.println("data written");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			FileReader fr=new FileReader("file2.txt");
+			int x;
+			while ((x=fr.read())!=-1) {
+				System.out.print((char)x);
+			}
+			fr.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
---
+++ java_22_may/src/com/fileIo/FileDemo.java
@@ -0,0 +1,38 @@
+package com.fileIo;
+
+import java.io.File;
+import java.io.IOException;
+
+/*
+ * File is a class
+ */
+class FDemo{
+	File file;
+	
+	public FDemo() {
+		file=new File("file33.txt");
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		display();
+
+	}
+	public void display() {
+		System.out.println("is file or not :"+file.isFile());
+		System.out.println("is directory or not :"+file.isDirectory());
+		System.out.println("can write or not :"+file.canWrite());
+		System.out.println("can read or not :"+file.canRead());
+		System.out.println("file name :"+file.getName());
+		System.out.println("file path :"+file.getPath());
+		System.out.println("get absolute file path :"+file.getAbsolutePath());
+	}
+}
+public class FileDemo {
+	public static void main(String[] args) {
+		new FDemo();
+	}
+
+}
